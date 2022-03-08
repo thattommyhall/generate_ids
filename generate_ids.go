49e88531ca5600d7ce8df495346b937d70c58af6
thattommyhall <thattommyhall@gmail.com>
2022-03-08 23:19:46 +0000

    squirt keys into SSM

--- generate_ids.go
+++ generate_ids.go
@@ -6,12 +6,18 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/hydra-booster/idgen"
 )
 
 func main() {
+
+	sess := session.Must(session.NewSession())
+	ssmClient := ssm.New(sess)
 
 	idCount := 1
 	if len(os.Args) > 1 {
@@ -26,18 +32,28 @@ func main() {
 		// priv, _, err := crypto.GenerateKeyPair(crypto.RSA, 2048)
 
 		if err != nil {
-			fmt.Println(fmt.Errorf("Failed to generate Peer ID: %w", err))
+			fmt.Println(fmt.Errorf("failed to generate Peer ID: %w", err))
 		}
 		pub := priv.GetPublic()
 		b, err := crypto.MarshalPrivateKey(priv)
 		if err != nil {
-			fmt.Println(fmt.Errorf("Failed to extract private key bytes: %w", err))
+			fmt.Println(fmt.Errorf("failed to extract private key bytes: %w", err))
 		}
 		privStr := base64.StdEncoding.EncodeToString(b)
 		id, _ := peer.IDFromPublicKey(pub)
 		idStr := id.Pretty()
-		fmt.Println(idStr)
-		fmt.Println(privStr)
-	}
 
+		ssmKey := "/bifrost-infra/peerid_to_privkey/" + idStr
+		_, err = ssmClient.PutParameter(&ssm.PutParameterInput{
+			Name:  &ssmKey,
+			Value: &privStr,
+			Type:  aws.String(ssm.ParameterTypeSecureString),
+		})
+
+		if err != nil {
+			fmt.Printf("Failed to upload %s to SSM: %s\n", idStr, err.Error())
+		} else {
+			fmt.Println(idStr)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/thattommyhall/generate_ids
 go 1.14
 
 require (
+	github.com/aws/aws-sdk-go v1.43.13
 	github.com/libp2p/go-libp2p-core v0.5.3
 	github.com/libp2p/hydra-booster v0.3.4
 )
